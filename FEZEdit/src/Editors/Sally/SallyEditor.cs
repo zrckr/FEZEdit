94ff504a7d449e85d361d440cbed389f6beb4363
zerocker <zerocloude@gmail.com>
2025-10-17 22:48:22 +0900

    sally-editor-wip-1

---
+++ FEZEdit/src/Editors/Sally/SallyEditor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FEZEdit.Content;
+using FEZEdit.Core;
+
+namespace FEZEdit.Editors.Sally;
+
+public partial class SallyEditor : Editor
+{
+    public override event Action ValueChanged;
+
+    public override object Value
+    {
+        get => _saveData;
+        set => _saveData = (SaveData)value;
+    }
+    
+    public override bool Disabled
+    {
+        set => throw new NotImplementedException();
+    }
+    
+    private SaveData _saveData;
+}
---
+++ FEZEdit/src/Extensions/BinaryExtensions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using FEZRepacker.Core.Definitions.Game.Level;
+using FEZRepacker.Core.Definitions.Game.XNA;
+
+namespace FEZEdit.Extensions;
+
+public static class BinaryExtensions
+{
+    #region Reading
+    
+    public static string ReadNullableString(this BinaryReader reader)
+    {
+        return reader.ReadBoolean()
+            ? reader.ReadString()
+            : null;
+    }
+
+    public static float? ReadNullableSingle(this BinaryReader reader)
+    {
+        return reader.ReadBoolean()
+            ? reader.ReadSingle()
+            : null;
+    }
+
+    public static Vector3 ReadVector3(this BinaryReader reader)
+    {
+        return new Vector3(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+    }
+
+    public static TimeSpan ReadTimeSpan(this BinaryReader reader)
+    {
+        return new TimeSpan(reader.ReadInt64());
+    }
+
+    public static TrileEmplacement ReadTrileEmplacement(this BinaryReader reader)
+    {
+        return new TrileEmplacement(reader.ReadInt32(), reader.ReadInt32(), reader.ReadInt32());
+    }
+    
+    #endregion
+
+    #region Writing
+
+    public static void WriteNullable(this BinaryWriter writer, string @string)
+    {
+        var nonEmpty = !string.IsNullOrEmpty(@string);
+        writer.Write(nonEmpty);
+        if (nonEmpty)
+        {
+            writer.Write(@string);
+        }
+    }
+    
+    public static void WriteNullable(this BinaryWriter writer, float? single)
+    {
+        writer.Write(single.HasValue);
+        if (single.HasValue)
+        {
+            writer.Write(single.Value);
+        }
+    }
+
+    public static void Write(this BinaryWriter writer, Vector3 vector)
+    {
+        writer.Write(vector.X);
+        writer.Write(vector.Y);
+        writer.Write(vector.Z);
+    }
+
+    public static void Write(this BinaryWriter writer, TimeSpan timeSpan)
+    {
+        writer.Write(timeSpan.Ticks);
+    }
+
+    public static void Write(this BinaryWriter writer, TrileEmplacement emplacement)
+    {
+        writer.Write(emplacement.X);
+        writer.Write(emplacement.Y);
+        writer.Write(emplacement.Z);
+    }
+
+    #endregion
+}
