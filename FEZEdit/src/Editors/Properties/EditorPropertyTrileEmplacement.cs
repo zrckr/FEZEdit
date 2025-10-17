c0c7c0210f278e06d01b1e937a2f40c345d45723
zerocker <zerocloude@gmail.com>
2025-10-17 21:48:56 +0900

    [Editors/Properties] Add support for trile emplacements

---
+++ FEZEdit/src/Editors/Properties/EditorPropertyTrileEmplacement.cs
@@ -0,0 +1,36 @@
+﻿using FEZRepacker.Core.Definitions.Game.Level;
+
+namespace FEZEdit.Editors.Properties;
+
+public partial class EditorPropertyTrileEmplacement : EditorPropertyVector<TrileEmplacement>
+{
+    private static readonly string[] NodePaths = ["%SpinBoxX", "%SpinBoxY", "%SpinBoxZ"];
+
+    protected override string[] Components => NodePaths;
+    
+    protected override TrileEmplacement GetValueInternal()
+    {
+        return new TrileEmplacement(
+            x: (int)_spinBoxes[0].Value,
+            y: (int)_spinBoxes[1].Value,
+            z: (int)_spinBoxes[2].Value
+        );
+    }
+
+    protected override void SetValueInternal(TrileEmplacement value)
+    {
+        _spinBoxes[0].Value = value.X;
+        _spinBoxes[1].Value = value.Y;
+        _spinBoxes[2].Value = value.Z;
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+        foreach (var spinBox in _spinBoxes)
+        {
+            spinBox.MinValue = int.MinValue;
+            spinBox.MaxValue = int.MaxValue;
+        }
+    }
+}
