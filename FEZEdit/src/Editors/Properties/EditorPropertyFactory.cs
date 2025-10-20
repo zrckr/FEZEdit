944bf19302dc648d94ef08db44a094b415bdaa87
zerocker <zerocloude@gmail.com>
2025-10-20 22:02:24 +0900

    [Editors/Properties] Detect array types separately in the factory

--- FEZEdit/src/Editors/Properties/EditorPropertyFactory.cs
+++ FEZEdit/src/Editors/Properties/EditorPropertyFactory.cs
@@ -2,12 +2,15 @@
 using System.Reflection;
 using Godot;
 using Godot.Collections;
+using Serilog;
 
 namespace FEZEdit.Editors.Properties;
 
 [Tool]
 public partial class EditorPropertyFactory: Resource
 {
+    private static readonly ILogger Logger = LoggerFactory.Create<EditorPropertyFactory>();
+    
     [Export] private Dictionary<string, PackedScene> _editorProperties = new();
     
     [Export] private PackedScene _defaultEditorProperty;
@@ -16,9 +19,12 @@ public partial class EditorPropertyFactory: Resource
     
     [Export] private PackedScene _nullableEditorProperty;
 
+    [Export] private PackedScene _arrayEditorProperty;
+
     public EditorProperty GetEditorProperty(Type type)
     {
-        var scene = _defaultEditorProperty;
+        PackedScene scene;
+
         var typeName = type.ToString().Split('`')[0];
         if (type.IsValueType && Nullable.GetUnderlyingType(type) != null)
         {
@@ -28,9 +34,18 @@ public partial class EditorPropertyFactory: Resource
         {
             scene = _enumEditorProperty;
         }
+        else if (type.IsArray)
+        {
+            scene = _arrayEditorProperty;
+        }
         else if (_editorProperties.TryGetValue(typeName, out var typeScene))
         {
             scene = typeScene;
+        }
+        else
+        {
+            scene = _defaultEditorProperty;
+            Logger.Warning("Unknown editor property type '{0}'", type.Name);
         }
         
         var instance = scene.Instantiate<EditorProperty>();
